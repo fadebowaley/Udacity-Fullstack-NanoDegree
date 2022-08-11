1cfe1c61247a5742e73a5aff4434a31960cf3201
fadebowaley <fadebowaley@gmail.com>
2022-08-11 22:31:14 +0100

    Project one - fyyur

--- config.py
+++ config.py
@@ -7,11 +7,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
-#Sqlalchemy database URI settings
-
-
-
-# TODO IMPLEMENT DATABASE URL
+SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:root@localhost:5432/fyyur_db"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:root@localhost:5432/fyyur"
-# SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
